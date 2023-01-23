e2dcb19583f2c8a7f04c69342916804c7163d9ea
Wentcont <pershin.dima2003@gmail.com>
2023-01-23 10:16:32 +0300

    Add first design to window

---
+++ StudyApp/View/LoginView.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace StudyApp.View
+{
+    public partial class LoginView : Window
+    {
+        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(passwordBox.Password) && passwordBox.Password.Length > 0)
+                textPassword.Visibility = Visibility.Collapsed;
+            else
+                textPassword.Visibility = Visibility.Visible;
+        }
+
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            passwordBox.Focus();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEmail.Text) && !string.IsNullOrEmpty(passwordBox.Password))
+            {
+                MessageBox.Show("Successfully Signed In");
+            }
+        }
+
+        private void txtEmail_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0)
+                textEmail.Visibility = Visibility.Collapsed;
+            else
+                textEmail.Visibility = Visibility.Visible;
+        }
+
+        private void textEmail_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtEmail.Focus();
+        }
+
+        private void LoginView_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var res = MessageBox.Show("Вы действительно хотите выйти из приложения?",
+                "Выход из приложения", MessageBoxButton.YesNo, MessageBoxImage.Question);
+        }
+
+    }
+}
