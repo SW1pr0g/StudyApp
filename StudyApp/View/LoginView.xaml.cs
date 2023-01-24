d753e6209f87e5b0f5ac157ed05228ea02de54af
SW1pr0g <poryadin_alex@mail.ru>
2023-01-24 23:15:44 +0300

    Update .gitignore, add Footer

--- StudyApp/View/LoginView.xaml.cs
+++ StudyApp/View/LoginView.xaml.cs
@@ -5,56 +5,27 @@ namespace StudyApp.View
 {
     public partial class LoginView : Window
     {
-        private void Border_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+
+        private void Header_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.ChangedButton == MouseButton.Left)
-                this.DragMove();
+            DragMove();
         }
 
-        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        
+        private void CollapseBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            
+            if (WindowState == WindowState.Maximized) WindowState = WindowState.Normal;
+            else WindowState = WindowState.Maximized;
         }
 
-        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        private void CloseBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            if (!string.IsNullOrEmpty(passwordBox.Password) && passwordBox.Password.Length > 0)
-                textPassword.Visibility = Visibility.Collapsed;
-            else
-                textPassword.Visibility = Visibility.Visible;
+            this.Close();
         }
 
-        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        private void HideBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            passwordBox.Focus();
+            WindowState = WindowState.Minimized;
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(txtEmail.Text) && !string.IsNullOrEmpty(passwordBox.Password))
-            {
-                MessageBox.Show("Successfully Signed In");
-            }
-        }
-
-        private void txtEmail_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0)
-                textEmail.Visibility = Visibility.Collapsed;
-            else
-                textEmail.Visibility = Visibility.Visible;
-        }
-
-        private void textEmail_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            txtEmail.Focus();
-        }
-
-        private void LoginView_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            var res = MessageBox.Show("Вы действительно хотите выйти из приложения?",
-                "Выход из приложения", MessageBoxButton.YesNo, MessageBoxImage.Question);
-        }
-
     }
 }
